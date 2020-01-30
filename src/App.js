8c2aed3be7b599ff39b8dd6639f8f48dc47344bd
unknown <dracsharp@gmail.com>
2020-01-30 14:21:17 +0000

    menus

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import "./pages/homepage/homepage.styles.scss";
 
 function App() {
   return (
     <div>
-      <div>Hello World</div>
+      <HomePage />
     </div>
   );
 }
