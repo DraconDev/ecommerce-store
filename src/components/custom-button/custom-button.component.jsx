63486cb3867dbff0a9eda06647b56748ef06fc8c
unknown <dracsharp@gmail.com>
2020-09-06 22:53:13 +0100

    ui fixes

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,11 +2,15 @@ import React from "react";
 
 // import "./custom-button.styles.scss";
 import { CustomButtonContainer } from "./custom-button.styles";
+import { Button } from "@material-ui/core";
 
 const CustomButton = ({ children, ...otherProps }) => {
-  return (
-    <CustomButtonContainer {...otherProps}>{children}</CustomButtonContainer>
-  );
+	return (
+		// <CustomButtonContainer {...otherProps}>{children}</CustomButtonContainer>
+		<Button {...otherProps} variant="outlined">
+			{children}
+		</Button>
+	);
 };
 
 export default CustomButton;
