bee70895dafe4cf1c899ca10626157c31f319af3
unknown <dracsharp@gmail.com>
2020-09-07 01:04:50 +0100

    2

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,70 +1,70 @@
-import React, { useEffect, useState } from "react";
-import { Route } from "react-router-dom";
-// import SHOP_DATA from "./shop.data";
-import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
-import CollectionPage from "../collection/collection.component";
+import React, { useEffect } from "react";
 // import {
 //   firestore,
 //   convertCollectionsShapshotToMap,
 // } from "../../firebase/firebase-utils";
 import { connect } from "react-redux";
+import { Route } from "react-router-dom";
+import { createStructuredSelector } from "reselect";
+// import SHOP_DATA from "./shop.data";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
 // import { updateCollections } from "../../redux/shop/shop.actions";
 import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
 import {
-  selectIsCollectionFetching,
-  selectIsCollectionsLoaded,
+	selectIsCollectionFetching,
+	selectIsCollectionsLoaded,
 } from "../../redux/shop/shop.selectors";
-import { createStructuredSelector } from "reselect";
-import WithSpinner from "../../components/with-spinner/with-spinner.component";
+import CollectionPage from "../collection/collection.component";
 
 const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
 const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 function ShopPage(props) {
-  // const { match, updateCollections, isCollectionFetching } = props;
-  const { match, isCollectionFetching, isCollectionLoaded } = props;
-  // const [state, setState] = useState({ loading: true });
-  // const loading = state.loading;
+	// const { match, updateCollections, isCollectionFetching } = props;
+	const { match, isCollectionFetching, isCollectionLoaded } = props;
+	// const [state, setState] = useState({ loading: true });
+	// const loading = state.loading;
 
-  // console.log("match", match);
-  // console.log("loading", state.loading, loading); 
+	// console.log("match", match);
+	// console.log("loading", state.loading, loading);
 
-  // useEffect(() => {
-  //   const collectionRef = firestore.collection("collections");
+	// useEffect(() => {
+	//   const collectionRef = firestore.collection("collections");
 
-  //   // collectionRef.onSnapshot(async (snapshot) => {
-  //   //   const collectionsMap = convertCollectionsShapshotToMap(snapshot);
-  //   //   // console.log("collectionsMap", Object.values(collectionsMap));
-  //   //   updateCollections(Object.values(collectionsMap));s
-  //   //   setState({ ...state, loading: false });
-  //   // });
+	//   // collectionRef.onSnapshot(async (snapshot) => {
+	//   //   const collectionsMap = convertCollectionsShapshotToMap(snapshot);
+	//   //   // console.log("collectionsMap", Object.values(collectionsMap));
+	//   //   updateCollections(Object.values(collectionsMap));s
+	//   //   setState({ ...state, loading: false });
+	//   // });
 
-  //   // fetch(
-  //   //   "https://firestore.googleapis.com/v1/projects/shop-db-4088c/databases/(default)/documents/collections"
-  //   // )
-  //   //   .then((response) => response.json())
-  //   //   .then((collections) => console.log("test", collections));
+	//   // fetch(
+	//   //   "https://firestore.googleapis.com/v1/projects/shop-db-4088c/databases/(default)/documents/collections"
+	//   // )
+	//   //   .then((response) => response.json())
+	//   //   .then((collections) => console.log("test", collections));
 
-  //   collectionRef.get().then(async (snapshot) => {
-  //     const collectionsMap = convertCollectionsShapshotToMap(snapshot);
-  //     // console.log("collectionsMap", Object.values(collectionsMap));
-  //     updateCollections(Object.values(collectionsMap));
-  //     setState({ ...state, loading: false });
-  //   });
-  // }, []);
+	//   collectionRef.get().then(async (snapshot) => {
+	//     const collectionsMap = convertCollectionsShapshotToMap(snapshot);
+	//     // console.log("collectionsMap", Object.values(collectionsMap));
+	//     updateCollections(Object.values(collectionsMap));
+	//     setState({ ...state, loading: false });
+	//   });
+	// }, []);
 
-  useEffect(() => {
-    const { fetchCollectionsStartAsync } = props;
-    fetchCollectionsStartAsync();
-  }, []);
+	useEffect(() => {
+		const { fetchCollectionsStartAsync } = props;
+		fetchCollectionsStartAsync();
+	}, [props]);
 
-  useEffect(() => {
-    // console.log("state changed", state);
-  }, []);
+	useEffect(() => {
+		// console.log("state changed", state);
+	}, []);
 
-  return (
-    <div className="shop-page">
-      {/* <Route
+	return (
+		<div className="shop-page">
+			{/* <Route
         exact
         path={`${match.path}`}
         loading={"loading"}
@@ -73,28 +73,28 @@ function ShopPage(props) {
 
       <Route path={`${match.path}/:collectionId`} component={CollectionPage} /> */}
 
-      <Route
-        exact
-        path={`${match.path}`}
-        render={(props) => (
-          <CollectionsOverviewWithSpinner
-            isLoading={isCollectionFetching}
-            {...props}
-          />
-        )}
-      />
+			<Route
+				exact
+				path={`${match.path}`}
+				render={(props) => (
+					<CollectionsOverviewWithSpinner
+						isLoading={isCollectionFetching}
+						{...props}
+					/>
+				)}
+			/>
 
-      <Route
-        path={`${match.path}/:collectionId`}
-        render={(props) => (
-          <CollectionPageWithSpinner
-            isLoading={!isCollectionLoaded}
-            {...props}
-          />
-        )}
-      />
-    </div>
-  );
+			<Route
+				path={`${match.path}/:collectionId`}
+				render={(props) => (
+					<CollectionPageWithSpinner
+						isLoading={!isCollectionLoaded}
+						{...props}
+					/>
+				)}
+			/>
+		</div>
+	);
 }
 
 // const mapDispatchToProps = (dispatch) => {
@@ -105,13 +105,13 @@ function ShopPage(props) {
 // };
 
 const mapStateToProps = createStructuredSelector({
-  isCollectionFetching: selectIsCollectionFetching,
-  isCollectionLoaded: selectIsCollectionsLoaded,
+	isCollectionFetching: selectIsCollectionFetching,
+	isCollectionLoaded: selectIsCollectionsLoaded,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
-  // fetchCollectionsStartAsync: () => {},
+	fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
+	// fetchCollectionsStartAsync: () => {},
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ShopPage);
